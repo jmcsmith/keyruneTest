4129167db20a8720dbe50a75a9fcacf76ed4bdd1
Joe Beaudoin <jmcsmith@gmail.com>
2025-01-23 09:05:47 -0500

    Initial Commit

---
+++ keyruneTest/keyruneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyruneTestApp.swift
+//  keyruneTest
+//
+//  Created by Joseph Beaudoin on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct keyruneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
