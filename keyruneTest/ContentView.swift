517cd0e5f48dbdbc745750f4acb301a80a10f6d6
Joe Beaudoin <jmcsmith@gmail.com>
2025-01-23 09:08:47 -0500

    example

--- keyruneTest/ContentView.swift
+++ keyruneTest/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    var unicodeValue = "&#xe665;"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(unicodeValue)
+                .font(Font.custom("keyrune", size: 30))
+            Text("&#xe665;")
+                .font(Font.custom("keyrune", size: 30))
         }
         .padding()
     }
